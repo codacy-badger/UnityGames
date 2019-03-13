a8579fce420dfa87ae4f1ce29c99115fd124e5a1
cainy-a <cainatkinson@gmail.com>
2019-03-13 19:12:03 +0000

    Added Loader Script, GameManager, Fixed Enemy Animation speed.

--- Rogue Cave Systems/Assets/Scripts/BoardManager.cs
+++ Rogue Cave Systems/Assets/Scripts/BoardManager.cs
@@ -1,6 +1,6 @@
-﻿using System;
+﻿using UnityEngine;
+using System;
 using System.Collections.Generic;
-using UnityEngine;
 using Random = UnityEngine.Random;
 public class BoardManager : MonoBehaviour
 {
@@ -34,7 +34,7 @@ public class BoardManager : MonoBehaviour
         {
             for (int y = 1; y < rows - 1; y++)
             {
-                gridPositions.Add(new Vector3(x, y));
+                gridPositions.Add(new Vector3(x, y, 0f));
             }
         }
     }
@@ -43,14 +43,13 @@ public class BoardManager : MonoBehaviour
         boardHolder = new GameObject("Board").transform;
         for (int x = -1; x < columns + 1; x++)
         {
-            for (int y = 1; y < rows + 1; y++)
+            for (int y = -1; y < rows + 1; y++)
             {
                 GameObject toInstantiate = floorTiles[Random.Range(0, floorTiles.Length)];
                 if (x == -1 || x == columns || y == -1 || y == rows)
-                {
                     toInstantiate = outerWallTiles[Random.Range(0, outerWallTiles.Length)];
-                }
-                GameObject instance = Instantiate(toInstantiate, new Vector3(x, y), Quaternion.identity) as GameObject;
+                GameObject instance =
+                    Instantiate(toInstantiate, new Vector3(x, y, 0f), Quaternion.identity) as GameObject;
                 instance.transform.SetParent(boardHolder);
             }
         }
@@ -80,6 +79,6 @@ public class BoardManager : MonoBehaviour
         LayoutObjectAtRandom(foodTiles, foodCount.minimum, foodCount.maximum);
         int enemyCount = (int)Mathf.Log(level, 2f);
         LayoutObjectAtRandom(enemyTiles, enemyCount, enemyCount);
-        Instantiate(exit, new Vector3(columns - 1, rows - 1), Quaternion.identity);
+        Instantiate(exit, new Vector3(columns - 1, rows - 1, 0f), Quaternion.identity);
     }
 }
--- Rogue Cave Systems/Assets/Scripts/GameManager.cs
+++ Rogue Cave Systems/Assets/Scripts/GameManager.cs
@@ -1,5 +1,27 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 public class GameManager : MonoBehaviour
 {
-    
+    public static GameManager instance = null;
+    private BoardManager boardScript;
+    private int level = 3;
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+    }
+    void InitGame()
+    {
+        boardScript.SetupScene(level);
+
+    }
 }
